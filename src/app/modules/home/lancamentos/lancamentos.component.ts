a481e1baaf674114aa303d8b1d984496a898756f
Daniel Mineiro <daniel.mineiro@castgroup.com.br>
2024-02-07 03:07:02 -0300

    feat: Implementação do home apenas para os Lançamentos

--- src/app/modules/home/lancamentos/lancamentos.component.ts
+++ src/app/modules/home/lancamentos/lancamentos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FiltroService } from 'src/app/core/services/filtro.service';
+
 
 @Component({
   selector: 'app-lancamentos',
@@ -6,5 +8,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./lancamentos.component.css']
 })
 export class LancamentosComponent {
+  pageSize: number = 5;
+  totalPages: number = 0;
+  imageDataList: any[] = [];
+  totalItems: number = 0;
 
+
+  filtro = {
+    cores: [],
+    caracteristicas: [],
+    ordenacao: 0
+  };
+
+  constructor(private filtroService: FiltroService) {
+    
+  }
+
+  ngOnInit(): void {
+    this.carregarProdutos();
+  }
+
+carregarProdutos(): void {
+    this.filtroService.listrarProdutosFiltrados(this.filtro, 0, this.pageSize)
+    .subscribe((response: any) => {
+      this.imageDataList = response.content;
+      this.totalPages = response.totalPages;
+      this.totalItems = response.totalElements;
+    })
+  }
+
+  getImageUrl(byteArray: Uint8Array): string {
+    const pathImage = "data:image/png;base64,";
+    return pathImage + byteArray;
+  }
 }
