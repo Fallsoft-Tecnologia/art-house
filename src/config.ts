f133e12aaa2cc4a57d2b3b0e84c6f2c43add496f
Daniel Mineiro <daniel.mineiro10@gmail.com>
2024-05-28 14:09:41 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ let apiUrl: string = '';
 if (local === dev) {
     apiUrl = 'http://localhost:8080/api';
 } else if(local === hml){
-    apiUrl = 'http://91.108.125.189:8080/api';
+    apiUrl = 'http://fallsoft.com.br:8080/api';
 }else if(local === prd){
     apiUrl = 'http://localhost:8080/api';
 
