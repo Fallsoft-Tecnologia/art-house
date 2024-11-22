e997aa50d5231cb9845e58eb76f5867b074e66be
Gabriel Andrade Almeida <59920810+g-andradd@users.noreply.github.com>
2024-11-22 19:40:57 -0300

    google tag on

--- src/app/core/whatsapp/whatsapp.component.ts
+++ src/app/core/whatsapp/whatsapp.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+declare var gtag: Function;
+
 @Component({
   selector: 'app-whatsapp',
   templateUrl: './whatsapp.component.html',
   styleUrls: ['./whatsapp.component.css']
 })
 export class WhatsappComponent {
-
+  trackWhatsappClick(): void {
+    if (typeof gtag !== 'undefined') {
+      gtag('event', 'conversion', {
+        'send_to': 'AW-661767471/b5OSCNvm9-wZEK-Kx7sC'
+      });
+    }
+  }
 }
